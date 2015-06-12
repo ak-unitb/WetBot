85c07d704e10d4d9be14aa1a1d6381aadb3da6b7
akr <alexis.kremin@freenet.de>
2015-06-12 16:36:36 +0200

    organizing the files; second trial

--- YunTimeSync.h
+++
@@ -1,9 +0,0 @@
-/*
-  YùnTimeSync - Implemntation to sync the time from the linino-/Yùn-side
-  Created by count0, June 11, 2015.
-  Released into the public domain.
-*/
-
-void initAndSyncTime();
-char* digitalClockDisplay();
-char* toDigits(int digits, bool colon);
