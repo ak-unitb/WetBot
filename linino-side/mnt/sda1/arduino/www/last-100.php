a26d32755064ef1e0e398a3950078db0b31fb9d7
akr <alexis.kremin@freenet.de>
2015-06-25 16:28:26 +0200

    db optimisations: indexes for sensor_datas and sql query optimisation for last-100.php

--- linino-side/mnt/sda1/arduino/www/last-100.php
+++ linino-side/mnt/sda1/arduino/www/last-100.php
@@ -7,33 +7,31 @@
 <?php
 	if ($conn) {
 
-		$sql = "SELECT `sd`.*, `s`.`name` as `sensor_name` FROM `sensor_datas` sd, `sensors` s";
+                $sql = "SELECT * FROM `sensor_datas`";
 
-		$whereClause = array();
-		if (!empty($_GET['grade_of_dryness'])) {
-			$whereClause[] = " `sd`.`grade_of_dryness` = " . intval($_GET['grade_of_dryness']) . "";
-		}
-		if (!empty($_GET['comment'])) {
-			$whereClause[] = " `sd`.`comment` = '" . stripslashes($_GET['comment']) . "'";
-		}
-		if (!empty($_GET['sensor_id'])) {
-			$whereClause[] = " `sd`.`sensor_id` = " . intval($_GET['sensor_id']) . "";
-		}
-		
-		if (!empty($whereClause)) {
-			$sql .= " WHERE " . implode(' AND ', $whereClause) . " AND `sd`.`sensor_id` = `s`.`id`";
-		} else {
-			$sql .= " WHERE `sd`.`sensor_id` = `s`.`id`";
-		}
-		$countLastEnrties = 720;
-		$sql .= " ORDER BY `sd`.`created_at` DESC LIMIT " . $countLastEnrties;
+                $whereClause = array();
+                if (!empty($_GET['grade_of_dryness'])) {
+                        $whereClause[] = " `grade_of_dryness` = " . intval($_GET['grade_of_dryness']) . "";
+                }
+                if (!empty($_GET['comment'])) {
+                        $whereClause[] = " `comment` = '" . stripslashes($_GET['comment']) . "'";
+                }
+                if (!empty($_GET['sensor_id'])) {
+                        $whereClause[] = " `sensor_id` = " . intval($_GET['sensor_id']) . "";
+                }
 
-		$rs = $conn->query($sql);
-		if ($rs === false) {
-			trigger_error('Wrong SQL: ' . $sql . ' Error: ' . $conn->error, E_USER_ERROR);
-		} else {
-			$rs->data_seek(0);
-			if ($rs->num_rows <= 0) {
+                if (!empty($whereClause)) {
+                        $sql .= " WHERE " . implode(' AND ', $whereClause) . "";
+                }
+                $countLastEnrties = 720;
+                $sql .= " ORDER BY `created_at` DESC LIMIT " . $countLastEnrties;
+
+                $rs = $conn->query($sql);
+                if ($rs === false) {
+                        trigger_error('Wrong SQL: ' . $sql . ' Error: ' . $conn->error, E_USER_ERROR);
+                } else {
+                        $rs->data_seek(0);
+                        if ($rs->num_rows <= 0) {
 ?>
 		<p class="text-message bg-warning">Keine Datensätze gefunden. Bitte prüfen Sie ggf. die Filter-Angaben.<p>
 <?php
@@ -76,12 +74,24 @@
 								<td data-th="Sensor-ID">
 									<select class="form-control" name="sensor_id" disabled>
 										<option value="">-</option>
-										<option value="1" <?php echo !empty($_GET['sensor_id']) && $_GET['sensor_id'] == 1 ? 'selected' : ''; ?>>1</option>
-										<option value="2" <?php echo !empty($_GET['sensor_id']) && $_GET['sensor_id'] == 2 ? 'selected' : ''; ?>>2</option>
-										<option value="3" <?php echo !empty($_GET['sensor_id']) && $_GET['sensor_id'] == 3 ? 'selected' : ''; ?>>3</option>
-										<option value="4" <?php echo !empty($_GET['sensor_id']) && $_GET['sensor_id'] == 4 ? 'selected' : ''; ?>>4</option>
-										<option value="5" <?php echo !empty($_GET['sensor_id']) && $_GET['sensor_id'] == 5 ? 'selected' : ''; ?>>5</option>
-										<option value="6" <?php echo !empty($_GET['sensor_id']) && $_GET['sensor_id'] == 6 ? 'selected' : ''; ?>>6</option>
+<?php
+                        $sqlSensors = 'SELECT *  FROM `sensors` WHERE `active` = TRUE;';
+                        $rsSensors = $conn->query($sqlSensors);
+                        $mapSensorIdName = array();
+
+                        if ($rsSensors === false) {
+                                trigger_error('Wrong SQL: ' . $sqlSensors . ' Error: ' . $conn->error, E_USER_ERROR);
+                        } else {
+                                $currentSensorId = $_GET['sensor_id'];
+                                $rsSensors->data_seek(0);
+                                while ($rowSensors = $rsSensors->fetch_assoc()) {
+                                $mapSensorIdName[$rowSensors['id']] = $rowSensors['name'];
+?>
+                                                                                <option value="<?php echo $rowSensors['id'] ?>" <?php echo $currentSensorId == $ro
+<?php
+                                }
+                        }
+?>
 									</select>
 								</td>
 								<td data-th="Aktion"><button type="submit" class="btn btn-primary pull-right">Filter anwenden</button></td>
@@ -95,7 +105,7 @@
 								<td data-th="Frequenz" class="hidden-xs"><?php echo $row['frequency'] ?> Hz</td>
 								<td data-th="Grad der Trockenheit"><?php echo $row['grade_of_dryness'] ?></td>
 								<td data-th="Kommentar" class="hidden-xs"><?php echo $row['comment'] ?></td>
-								<td data-th="Sensor-Name"><?php echo $row['sensor_name'] ?></td>
+								<td data-th="Sensor-Name"><?php echo $mapSensorIdName[$row['sensor_id']] ?></td>
 								<td data-th="am/um"><?php echo $row['created_at'] ?></td>
 							</tr>
 <?php
